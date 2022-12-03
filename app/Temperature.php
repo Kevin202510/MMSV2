73db02de040130b1c7eabc5d1575014fa5c2f57b
KevinCaluag20 <silent.kevin.f.caluag20@gmail.com>
2022-12-03 14:25:49 +0800

    ASd

--- app/Temperature.php
+++ app/Temperature.php
@@ -7,5 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Temperature extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'temperature','status',
+    ];
+
+    protected $appends = [
+        'date',
+        'time',
+        'statusName',
+    ];
+
+    public function getDateAttribute()
+    {
+        return $this->created_at? $this->created_at->format('M-d-Y'): null;
+    }
+
+    public function getTimeAttribute()
+    {
+        return $this->created_at? $this->created_at->format('g:i:s A'): null;
+    }
+
+    public function getstatusNameAttribute()
+    {
+        $stat="";
+        if($this->status==0){
+            $stat = "High Temperature";
+        }else if($this->status==1){
+            $stat = "Good Temperature";
+        }else if($this->status==2){
+            $stat = "Low Temperature";
+        }
+        return $stat;
+    }
 }
