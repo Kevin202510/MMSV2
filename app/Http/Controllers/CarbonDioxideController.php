587238ac40919f9ccc5ecf4bff26142e8468716a
KevinCaluag20 <silent.kevin.f.caluag20@gmail.com>
2022-12-03 12:40:07 +0800

    Asd

---
+++ app/Http/Controllers/CarbonDioxideController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarbonDioxideController extends Controller
+{
+    //
+}
---
+++ database/seeders/LightsSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class LightsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
